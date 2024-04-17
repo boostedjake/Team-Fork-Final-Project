c8269f261c41d786fe5393711ea4e49d5314fffa
outwove <165950748+outwove@users.noreply.github.com>
2024-04-16 23:23:00 -0500

    Add files via upload

--- duckSongCode.ino
+++ duckSongCode.ino
@@ -1,7 +1,7 @@
-
+//#include "melody1"
 #include <Servo.h>   
 
-Servo myservo; // initialize servo
+Servo myservo; // initialize servos
 Servo myservo2;
 
 //initialize the wire for the "button"
@@ -9,6 +9,7 @@ const int buttonPin = 2;
 const int piezoPin = 3;
 const int man = 4;
 const int duck = 5;
+const int duckStand = 13;
 const int startButton = 6;
 const int ledStand = 7;
 const int ledHouse = 8;
@@ -19,16 +20,16 @@ const int ledStartButton = 10;
 //const int duckHome = 6;
 // const int manStore = 8;
 
-// myservo goes to 9
+const int servo1 = 11;
 // myservo2 goes to 10
 
-const int dialogueButton = 11;
+//const int dialogueButton = 11;
 const int storeGrapes = 12;
-const int ledDialogueButton = 13;
+// const int ledDialogueButton = 13;
 
 
 
-State change Detection / variables to change
+//State change Detection / variables to change
 int buttonState;
 //int lastButtonState = 0;
 int actionVal;
@@ -40,15 +41,16 @@ void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(man, INPUT);
   pinMode(duck, INPUT);
-  pinMode(duckStore, INPUT);
+  pinMode(duckStand, INPUT);
   pinMode(startButton, INPUT);
-  pinMode(dialogueButton, INPUT);
+  // pinMode(dialogueButton, INPUT);
   pinMode(storeGrapes, INPUT);
   pinMode(ledHouse, OUTPUT);
   pinMode(ledStand, OUTPUT);
   pinMode(ledStore, OUTPUT);
   pinMode(ledStartButton, OUTPUT);
-  pinMode(ledDialogueButton, OUTPUT);
+  // pinMode(ledDialogueButton, OUTPUT);
+  myservo.attach(servo1);
   pinMode(piezoPin, OUTPUT);
 
   Serial.begin(9600); //starts the serial communication
@@ -59,29 +61,24 @@ void loop() {
   
   preInteraction();
 
-  if (digitalRead(startButton) == HIGH && digitalRead(dialogueButton) == HIGH) {
-    
+  if (digitalRead(startButton) == HIGH) {
+    //set button +1
+    buttonState++;
+    interactionOne();
+    interactionThree();
+    //interactionFive
   }
-  interactionOne();
-  
-  if (doStart && actionVal == 2) { 
-    //
-    actionVal = 3;
-  }
-  
-  if (doStart && actionVal == 3) { 
-    //
-    actionVal = 4;
+  interactionTwo();
+  interactionTwelve();
+  interactionThirteen();
 
-  }
- 
 }
 
 void preInteraction() {
-  //iterate through each action depending on the value of actionVal
+
   //player places duck and man in starting location
   if (digitalRead(duck) == HIGH && actionVal == 0 && digitalRead(man) == HIGH) { 
-    //start led blinks
+    //start led lights up
     //action value set to 1
     actionVal = 1;
     digitalWrite(ledStartButton, HIGH);
@@ -90,13 +87,82 @@ void preInteraction() {
 }
 
 void interactionOne() { 
-    if (buttonState = 1 && actionVal == 1 && digitalRead(duck) == HIGH && digitalRead(man) == HIGH) {   
-    //led turn off
+
+  //player presses button
+  if (buttonState == 1 && actionVal == 1 && digitalRead(duck) == HIGH && digitalRead(man) == HIGH) {   
+    //led start button turns off
+    //led stand turns on
+    //sound effect plays
     //action value 2
-    //
     digitalWrite(ledStartButton, LOW);
+    digitalWrite(ledStand, HIGH);
+    soundEffects();
     actionVal = 2;
+    
+  }
+}  
+
+void interactionTwo() { 
+  //Player places duck at the lemonade stand
+  if (actionVal == 2 && digitalRead(duckStand) == HIGH && digitalRead(man) == HIGH) {
+    
+    //button led prompts
+    //action value 3
+    digitalWrite(ledStartButton, HIGH);
+    digitalWrite(ledStand, LOW);
+    actionVal = 3;
 
   }
+}
+
+void interactionThree() {
+  //Player presses button second time dialogue servo flips to grape
+  if (buttonState == 2 && actionVal == 3 && digitalRead(duckStand) == HIGH && digitalRead(man) == HIGH) {
+    //start button off
+    //servo flips "grapes"
+    digitalWrite(ledStartButton, LOW);
+    myservo.write(180);
+  }
+  else {
+    //move servo back after something changes
+    myservo.write(0);
+  }
+  //sound effects play
+  //store LED lights up
+  soundEffects();
+  digitalWrite(ledStore, HIGH);
+  actionVal = 4;
+}
+
+void interactionTwelve() {
+  if (actionVal == 4 && digitalRead(duck) == HIGH && digitalRead(man) == HIGH) {
+    digitalWrite(ledStore, LOW);
+    actionVal = 5;
+  }
+}
+
+void interactionThirteen() {
+  if (actionVal == 5 && digitalRead(duck) == HIGH && digitalRead(man) == HIGH && digitalRead(storeGrapes) == HIGH) {
+    soundEffects();
+    actionVal = 6;
+  }
+
+}
+
+
+
+void soundEffects() {
+  if (actionVal == 1 || actionVal == 5) {
+    tone(3, 10);
+    noTone(3);
+  }
+  if (actionVal == 3) { 
+    tone(3, 10);
+    noTone(3);
+  }
+  // if (actionVal == #) {
+
+  // }
+
 }
 
